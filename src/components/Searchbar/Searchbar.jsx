11c0e5e2fcabc896f8177245ea383859e5832875
YurecUA <81627173+YurecUA@users.noreply.github.com>
2022-12-29 15:39:06 +0200

    Create Searchbar.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Searchbar.module.css'
+import { toast } from 'react-toastify';
+
+
+export default class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ query: e.target.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.query.trim() === '') {
+      toast.error('Enter your search query');
+      return;
+    }
+
+    this.props.onSubmit(this.state.query);
+  };
+
+  render() {
+    return (
+      <header className={styles.searchbar}>
+        <form className={styles.searchForm} onSubmit={this.handleSubmit}>
+          <input
+            onInput={this.handleInputChange}
+            className={styles.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value = {this.state.query}
+          />
+          <button type="submit" className={styles.searchFormButton}>
+            search
+          </button>
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
