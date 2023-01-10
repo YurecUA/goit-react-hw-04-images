6263384814ed3a087f8caa8c8fccbff5c258e577
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-10 15:51:47 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Searchbar from './Searchbar/Searchbar';
+import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import Loader from './Loader/Loader';
 import Button from './Button/Button';
-import Modal from './Modal/Modal';
+import { Modal } from './Modal/Modal';
 
+const API_KEY = '25766392-01b12b6ed5ab34bc2910d9c3e';
+const URL = 'https://pixabay.com/api/';
 
-export default class App extends Component {
-  state = {
-    URL: 'https://pixabay.com/api/',
-    API_KEY: '25766392-01b12b6ed5ab34bc2910d9c3e',
-    pictures: [],
-    error: '',
-    status: 'idle',
-    page: 1,
-    query: '',
-    totalHits: null,
-    largeImgURL: '',
-  };
+export const App = () => {
+  const [pictures, setPictures] = useState([]);
+  const [error, setError] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [totalHits, setTotalHits] = useState(null);
+  const [largeImgURL, setLargeImgURL] = useState('');
 
-  fetchImg = () => {
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    const fetchImg = () => {
+      setStatus('pending');
     return fetch(
-      `${this.state.URL}?q=${this.state.query}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      `${URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     )
       .then(res => {
         if (res.ok) {
@@ -35,58 +38,41 @@ export default class App extends Component {
         if (!pictures.total) {
           toast.error('Did find anything, mate');
         }
-        this.setState(prevState => {
-          return {
-            pictures: [...prevState.pictures, ...pictures.hits],
-            status: 'resolved',
-            totalHits: pictures.total,
-          };
-        });
+        setPictures((prevState) => {
+          return [...prevState, ...pictures.hits];
+        })
+        setTotalHits(pictures.total);
+        setStatus('fullfiled');
       })
-      .catch(error => this.setState({ error, status: 'rejected' }));
+      .catch(error => { setStatus('rejected'); setError(error)});
+    };
+    fetchImg();
+  }, [query, page])
+
+  const processSubmit = query => {
+    setPictures([]);
+    setPage(1);
+    setQuery(query);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.query !== prevState.query || prevState.page !== this.state.page) {
-      this.setState({ status: 'pending'});
-      this.fetchImg();
-    }
-    // if (
-    //   this.state.query === prevState.query &&
-    //   this.state.page !== prevState.page
-    // ) {
-    //   this.setState({ status: 'pending' });
-    //   this.fetchImg();
-    // }
+  const handleLoadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+
+  const setImageModalURL = (largeImgURL) => {
+  setLargeImgURL(largeImgURL)
   }
 
-  processSubmit = query => {
-    this.setState({ query, page: 1, pictures: [] });
-  };
-
-  handleLoadMore = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
-
-  setLargeImgURL = (largeImgURL) => {
-  this.setState({largeImgURL})
-  }
-
-  render() {
-    const { pictures, status, totalHits } = this.state;
     return (
       <>
-        <Searchbar onSubmit={this.processSubmit} />
-        {pictures.length > 0 && <ImageGallery images={pictures} setLargeImgURL = {this.setLargeImgURL} />}
+        <Searchbar onSubmit={processSubmit} />
+        {pictures.length > 0 && <ImageGallery images={pictures} setLargeImgURL = {setImageModalURL} />}
         {totalHits > pictures.length && (
-          <Button onClick={this.handleLoadMore} />
+          <Button onClick={handleLoadMore} />
         )}
         {status === 'pending' && <Loader />}
-        {this.state.largeImgURL && <Modal onClose={this.setLargeImgURL} pic={this.state.largeImgURL} />}
+        {largeImgURL && <Modal onClose={setImageModalURL} pic={largeImgURL} />}
         <ToastContainer autoClose={5000} />
       </>
     );
   }
-}
